05e0e04c57b796084631e8e234860ce381e84733
Fidget836 <109520884+Fidget836@users.noreply.github.com>
2024-09-09 15:36:46 +0200

    camagru 09/09 mailcatcher

--- web/backend/views/sendMail.php
+++ web/backend/views/sendMail.php
@@ -2,18 +2,9 @@
 error_log("Lancement du script d'envoi d'email");
 
 $to = 'thomas.marie836@gmail.com';
-$subject = 'Test';
-$message = 'Ceci est un test';
-$headers = 'From: thomas.marie836@gmail.com' . "\r\n" .
-           'Reply-To: thomas.marie836@gmail.com' . "\r\n" .
-           'X-Mailer: PHP/' . phpversion();
-
-// Validation des adresses email
-if (!filter_var($to, FILTER_VALIDATE_EMAIL)) {
-    error_log("L'adresse email destinataire est invalide.");
-    echo json_encode(['status' => 'error', 'message' => 'Adresse email invalide']);
-    exit();
-}
+$subject = 'Nouveau mail';
+$message = 'STP MARCHE';
+$headers = 'From: webmaster@example.com';
 
 if (mail($to, $subject, $message, $headers)) {
     error_log("Email envoyé avec succès à $to");
